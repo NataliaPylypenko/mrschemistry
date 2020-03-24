e540a15a10fae33ca32f6cf4de92c83937f6f6c5
Natalia <natalia28ivanivna@gmail.com>
2020-03-24 06:12:52 +0200

    message

---
+++ js/common.js
@@ -0,0 +1,81 @@
+(function(){
+    
+    function initSize() {
+        $(".box_menu .panel-heading").each(function() {
+            var ph = $(this).height() + 3;
+            var pdt = $(this).find(".dropdown-toggle");
+            pdt.height(ph);
+        });
+
+        $(".till_item .tc").each(function() {
+            var parh = $(this).parent().height();
+            $(this).height(parh);
+        });
+    };
+
+    initSize();
+
+    $(window).resize(function() {
+        initSize();
+    });
+
+
+
+
+  $(window).scroll(function () {
+      var top = $(document).scrollTop();
+      $('.splash').css({
+        'background-position': '0px -'+(top/3).toFixed(2)+'px'
+      });
+      if(top > 50)
+        $('#home > .navbar').removeClass('navbar-transparent');
+      else
+        $('#home > .navbar').addClass('navbar-transparent');
+  });
+
+  $("a[href='#']").click(function(e) {
+    e.preventDefault();
+  });
+
+  var $button = $("<div id='source-button' class='btn btn-success btn-md' title='Show source'> ≡≡ </div>").click(function(){
+    var html = $(this).parent().html();
+    html = cleanSource(html);
+    $("#source-modal pre").text(html);
+    $("#source-modal").modal();
+  });
+
+  $('.bs-component [data-toggle="popover"]').popover();
+  $('.bs-component [data-toggle="tooltip"]').tooltip();
+
+  $(".bs-component").hover(function(){
+    $(this).append($button);
+    $button.show();
+  }, function(){
+    $button.hide();
+  });
+
+  function cleanSource(html) {
+    var lines = html.split(/\n/);
+
+    lines.shift();
+    lines.splice(-1, 1);
+
+    var indentSize = lines[0].length - lines[0].trim().length,
+        re = new RegExp(" {" + indentSize + "}");
+
+    lines = lines.map(function(line){
+      if (line.match(re)) {
+        line = line.substring(indentSize);
+      }
+
+      return line;
+    });
+
+    lines = lines.join("\n");
+
+    return lines;
+  };
+
+
+
+})();
